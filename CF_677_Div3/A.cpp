75cdc08ec6fa2c45d625247006fa5e906aedda8a
Nitin Madhukar <iec2018053@iiita.ac.in>
2021-06-10 11:06:46 +0530

    cf_contest

--- CF_677_Div3/A.cpp
+++ CF_677_Div3/A.cpp
@@ -1,94 +1,56 @@
-/* Nitin Madhukar */
-#include<bits/stdc++.h>
-#include <algorithm>
+#include <bits/stdc++.h>  // This will work only for g++ compiler.
 
-#define ll              long long
-#define mp              make_pair
-#define lb              lower_bound
-#define ub              upper_bound
-#define all(x)          x.begin(), x.end()
-#define big(x)          greater<x>()
 
-#define sp              fixed<<setprecision
-#define vi              vector<int>
-#define vll             vector<ll>
-#define vvi             vector<vi>
-#define vvll            vector<vll>
-#define pi              pair<int,int>
-#define pll             pair<ll,ll>
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i) // 0 based indexing
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i) // 1 based indexing
+#define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i) // closed interver from l to r r inclusive
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i) // reverse 0 based.
+#define forr1(i, n) for (int i = (int)(n); i >= 1; --i) // reverse 1 based
 
-#define PI              3.14159265
-#define MOD             (int)1000000007
-#define LINF            LONG_MAX
-#define NL              LONG_MIN
-#define INF             INT_MAX
-#define NI              INT_MIN
+//sh    ort hand for usual tokens
+#define pb push_back
+#define fi first
+#define se second
 
-#define IOS()           ios_base::sync_with_stdio(0);cin.tie(0);
-#define deb(x)          cerr<<#x<<" : "<<x<<"\n";
-#define deball(x)       for(auto iit:x) cerr<<" "<<iit;cerr<<"\n";
-#define rep(i,b,c)      for(i=b; i<c; ++i)
-#define rrep(i,b,c)     for(i=b; i>=c; --i)
+// to be used with algorithms that processes a container Eg: find(all(c),42)
+#define all(x) (x).begin(), (x).end() //Forward traversal
+#define rall(x) (x).rbegin, (x).rend() //reverse traversal
 
-#define IN              cin >>
-#define OUT             cout <<
-#define endl            "\n"
-#define all(a)          (a).begin(), (a).end()
-#define allr(a)         (a).rbegin(), (a).rend()
-#define pb              push_back
-#define fi              first
-#define se              second
-#define write(a)          \
-    for (auto x : a)      \
-    {                     \
-        cout << x << " "; \
-    }                     \
-    cout << endl;
-#define read(a)       \
-    for (auto &x : a) \
-    {                 \
-        cin >> x;     \
-    }
+// find if a given value is present in a container. Container version. Runs in log(n) for set and map
+#define present(c,x) ((c).find(x) != (c).end())
+
+// Avoiding wrap around of size()-1 where size is a unsigned int.
+#define sz(a) int((a).size())
+
 
 using namespace std;
 
-void solve() {
-   ll n;cin>>n;
-   set<ll>st;
-   vector<ll> a(n);
-   for(auto &i:a){cin>>i;st.insert(i);}
-   queue<ll>q;
-   for(ll i=1;i<n;i++){
-       for(ll j=0;j<i;j++){
-           q.push(abs(a[i]-a[j]));
-       }
-   }
-   while(!q.empty() and st.size()<=300){
-       ll fr=q.front();
-       q.pop();
-       if(!st.count(fr)){
-           for(ll i=0;i<a.size();i++)q.push(abs(fr-a[i]));
-            a.push_back(fr);
-            st.insert(fr);
-       }
-   }
-   if(st.size()>300){
-       cout<<"NO\n";
-       return;
-   }
-   cout<<"YES\n";
-   cout<<st.size()<<endl;
-   for(auto i:st)cout<<i<<" ";
-   cout<<'\n';
-   return;
-}
-int main(){
-    IOS();
-    int t = 1;
-    cin >> t;
+// Shorthand for commonly used types
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+#define mod 1000000007
 
-    for (int i = 0; i < t; ++i)
-        solve();
 
-return 0;
+int main()
+{
+    ll tc ;
+    cin>>tc;
+    while (tc--)
+    {
+      int n;
+      cin>>n;
+      int l=trunc(log10(n)) + 1;
+      int r=n%10;
+      int ans=10*(r-1);
+    //   cout<<l;
+      ans+=l*(l+1)/2;
+       cout<<ans<<endl;
+    }
+    return 0;
 }
