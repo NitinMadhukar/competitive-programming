82372b6681ed0492db0feb34c9d212494fb0efa9
Nitin Madhukar <iec2018053@iiita.ac.in>
2021-06-19 20:46:37 +0530

    cf_contest_added

---
+++ CF_726_Div2/E1.cpp
@@ -0,0 +1,91 @@
+/* Nitin Madhukar */
+#include<bits/stdc++.h>
+#include <algorithm>
+
+#define ll              long long
+#define mp              make_pair
+#define lb              lower_bound
+#define ub              upper_bound
+#define all(x)          x.begin(), x.end()
+#define big(x)          greater<x>()
+
+#define sp              fixed<<setprecision
+#define vi              vector<int>
+#define vll             vector<ll>
+#define vvi             vector<vi>
+#define vvll            vector<vll>
+#define pi              pair<int,int>
+#define pll             pair<ll,ll>
+
+#define PI              3.14159265
+#define MOD             (int)1000000007
+#define LINF            LONG_MAX
+#define NL              LONG_MIN
+#define INF             INT_MAX
+#define NI              INT_MIN
+
+#define IOS()           ios_base::sync_with_stdio(0);cin.tie(0);
+#define deb(x)          cerr<<#x<<" : "<<x<<"\n";
+#define deball(x)       for(auto iit:x) cerr<<" "<<iit;cerr<<"\n";
+#define rep(i,b,c)      for(i=b; i<c; ++i)
+#define rrep(i,b,c)     for(i=b; i>=c; --i)
+
+#define IN              cin >>
+#define OUT             cout <<
+#define endl            "\n"
+#define all(a)          (a).begin(), (a).end()
+#define allr(a)         (a).rbegin(), (a).rend()
+#define pb              push_back
+#define fi              first
+#define se              second
+#define write(a)          \
+    for (auto x : a)      \
+    {                     \
+        cout << x << " "; \
+    }                     \
+    cout << endl;
+#define read(a)       \
+    for (auto &x : a) \
+    {                 \
+        cin >> x;     \
+    }
+
+using namespace std;
+
+string minstring(string pre,int k){
+	while(pre.size()<k){
+		pre+=pre;
+	}
+	while(pre.size()>k){
+		pre.pop_back();
+	}
+	return pre;
+}
+
+void solve() {
+	ll n,k;cin>>n>>k;
+	string a; cin>>a;
+	string pre="";
+	pre+=a[0];
+
+	string ans=minstring(pre,k);
+
+	for(ll i=1;i<n;i++){
+		if(a[i]>a[0])break;
+		pre+=a[i];
+		ans=min(ans,minstring(pre,k));
+	}
+	cout<<ans<<'\n';
+	return ;
+
+}
+int main(){
+    IOS();
+    ll t = 1;
+    // cin >> t;
+
+    for (int i = 0; i < t; ++i)
+        solve();
+
+return 0;
+}
