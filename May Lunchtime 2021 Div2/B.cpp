6835f849dfd03a32aa05b9c3793148cf94886e74
Nitin Madhukar <iec2018053@iiita.ac.in>
2021-06-21 14:50:03 +0530

    lunchtime added

---
+++ May Lunchtime 2021 Div2/B.cpp
@@ -0,0 +1,95 @@
+/* Nitin Madhukar */
+#include<bits/stdc++.h>
+#include <algorithm>
+
+#define ll              long long
+#define mp              make_pair
+#define lb              lower_bound
+#define ub              upper_bound
+#define all(x)          x.begin(), x.end()
+#define big(x)          greater<x>()
+
+#define sp              fixed<<setprecision
+#define vi              vector<int>
+#define vll             vector<ll>
+#define vvi             vector<vi>
+#define vvll            vector<vll>
+#define pi              pair<int,int>
+#define pll             pair<ll,ll>
+
+#define PI              3.14159265
+#define MOD             (int)1000000007
+#define LINF            LONG_MAX
+#define NL              LONG_MIN
+#define INF             INT_MAX
+#define NI              INT_MIN
+
+#define IOS()           ios_base::sync_with_stdio(0);cin.tie(0);
+#define deb(x)          cerr<<#x<<" : "<<x<<"\n";
+#define deball(x)       for(auto iit:x) cerr<<" "<<iit;cerr<<"\n";
+#define rep(i,b,c)      for(i=b; i<c; ++i)
+#define rrep(i,b,c)     for(i=b; i>=c; --i)
+
+#define IN              cin >>
+#define OUT             cout <<
+#define endl            "\n"
+#define all(a)          (a).begin(), (a).end()
+#define allr(a)         (a).rbegin(), (a).rend()
+#define pb              push_back
+#define fi              first
+#define se              second
+#define write(a)          \
+    for (auto x : a)      \
+    {                     \
+        cout << x << " "; \
+    }                     \
+    cout << endl;
+#define read(a)       \
+    for (auto &x : a) \
+    {                 \
+        cin >> x;     \
+    }
+
+using namespace std;
+
+void solve() {
+	ll n,k;
+	cin>>n>>k;
+	string s;cin>>s;
+	ll ans=0;
+	for(int i=1;i<s.length();i++){
+		if(s[i]==s[i-1])ans+=2;
+		else ans++;
+	}
+	vector<ll> qr(k);
+	read(qr);
+	// cout<<ans<<'\n';
+	for(auto x:qr){
+		x--;
+		if(x<n-1){
+			if(s[x]==s[x+1]){
+				ans--;
+			}
+			else
+				ans++;
+		}
+		if(x>0){
+			if(s[x]==s[x-1])
+				ans--;
+			else ans++;
+		}
+		s[x]^=1;
+		cout<<ans<<'\n';
+	}
+	return ;
+}
+int main(){
+    IOS();
+    ll t = 1;
+    cin >> t;
+
+    for (int i = 0; i < t; ++i)
+        solve();
+
+return 0;
+}
