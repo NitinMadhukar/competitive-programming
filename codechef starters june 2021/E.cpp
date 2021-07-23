c866554cf19a5f141397f5338220d3c3fa2474f6
Nitin Madhukar <iec2018053@iiita.ac.in>
2021-07-23 15:48:27 +0530

    starter june

---
+++ codechef starters june 2021/E.cpp
@@ -0,0 +1,87 @@
+/* Nitin Madhukar */
+#include<bits/stdc++.h>
+#include <algorithm>
+
+#define ll              long long
+#define mp              make_pair
+#define lb              lower_bound
+#define ub              upper_bound
+#define all(x)          x.begin(), x.end()
+#define big(x)          greater<x>()
+
+#define sp              fixed<<setprecision
+#define vi              vector<int>
+#define vll             vector<ll>
+#define vvi             vector<vi>
+#define vvll            vector<vll>
+#define pi              pair<int,int>
+#define pll             pair<ll,ll>
+#define vpll			vector<pair<ll,ll>>
+
+#define PI              3.14159265
+#define MOD             (int)1000000007
+#define LINF            LONG_MAX
+#define NL              LONG_MIN
+#define INF             INT_MAX
+#define NI              INT_MIN
+
+#define IOS()           ios_base::sync_with_stdio(0);cin.tie(0);
+#define deb(x)          cerr<<#x<<" : "<<x<<"\n";
+#define deball(x)       for(auto iit:x) cerr<<" "<<iit;cerr<<"\n";
+#define rep(i,b,c)      for(i=b; i<c; ++i)
+#define rrep(i,b,c)     for(i=b; i>=c; --i)
+
+#define IN              cin >>
+#define OUT             cout <<
+#define endl            "\n"
+#define all(a)          (a).begin(), (a).end()
+#define allr(a)         (a).rbegin(), (a).rend()
+#define pb              push_back
+#define fi              first
+#define se              second
+#define write(a)          \
+    for (auto x : a)      \
+    {                     \
+        cout << x << " "; \
+    }                     \
+    cout << endl;
+#define read(a)       \
+    for (auto &x : a) \
+    {                 \
+        cin >> x;     \
+    }
+
+using namespace std;
+
+void solve() {
+	ll n,h;
+	cin>>n>>h;
+	vpll arr(n);
+	for(ll i=0;i<n;i++)
+		cin>>arr[i].first;
+	for(ll i=0;i<n;i++){
+		cin>>arr[i].second;
+		arr[i].second-=h;
+	}
+	ll ans=0;
+	for(auto i:arr){
+		if(i.first<=i.second){
+			cout<<"-1\n";
+			return ;
+		}
+		ans=max(ans,(i.second+i.first+2)/2);
+	}
+	cout<<ans<<endl;
+
+
+}
+int main(){
+    IOS();
+    ll t = 1;
+    cin >> t;
+
+    for (int i = 0; i < t; ++i)
+        solve();
+
+return 0;
+}
